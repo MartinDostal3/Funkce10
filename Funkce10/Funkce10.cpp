3c08667d78b224e44131b32b2b9b603345f5e89a
dostalm.04 <dostalm.04@spst.eu>
2023-01-23 12:41:54 +0100

    Přidejte soubory projektu.

---
+++ Funkce10/Funkce10.cpp
@@ -0,0 +1,41 @@
+﻿// Funkce10.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+using namespace std;
+bool dokonaleCislo(int n)
+{
+    int soucetDelitelu{ 0 };
+    bool totoJeDokonaleCislo = false;
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            soucetDelitelu += i;
+        }
+    }
+    if (soucetDelitelu == n)
+    {
+        totoJeDokonaleCislo = true;
+    }
+    return totoJeDokonaleCislo;
+}
+int main()
+{
+    cout << "\nZadejte cele kladne cislo: ";
+    int a, b;
+
+    cin >> a;
+    cout << "\nZadejte cele kladne cislo: ";
+    cin >> b;
+    for (int i = a; i <= b; i++)
+    {
+
+        if (dokonaleCislo(i))cout << "\nDokonale cislo je: " << i;
+    }
+
+    getchar();
+    getchar();
+    return 0;
+
+}
